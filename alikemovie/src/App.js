7fa84799657eb140fd5edb5b63a3199b5e9766c2
MIINII <ft.dawn.o@gmail.com>
2022-08-26 15:03:01 +0900

    first commit

---
+++ alikemovie/src/App.js
@@ -0,0 +1,26 @@
+import React, { useSelector } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+
+const App = () => {
+  return (
+    <div>
+      {' '}
+      <Router />{' '}
+    </div>
+  );
+};
+
+const Router = () => {
+  const todos = useSelector(state => state.todos.todos);
+  console.log(todos);
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
