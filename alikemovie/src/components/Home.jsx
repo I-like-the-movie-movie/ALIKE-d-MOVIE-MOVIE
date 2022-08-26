9f6aa1abd10fb81f18c1d41dcd5c4a77372b3a44
MIINII <83333217+MIINII@users.noreply.github.com>
2022-08-26 22:31:20 +0900

    Home화면 만들기

--- alikemovie/src/components/Home.jsx
+++ alikemovie/src/components/Home.jsx
@@ -1,12 +1,20 @@
 import Reacts from 'react';
+import './Home.css';
+import Logo from '../assets/logo.svg';
+import OrangeArrow from '../assets/arrow_orange.svg';
 
 const Home = () => {
   return (
     <>
       <div className='home_wrap'>
-        <div className='movie_list'>지금까지의 영화들</div>
-        <div className='review_list'>리뷰쓰러 가기</div>
+        <img className='logo' src={Logo} alt='로고' />
+        <div className="list_button">
+          <button className='movie_list'>지금까지의 영화들</button>
+          <button className='review_list'>리뷰쓰러 가기</button>
+        </div>
       </div>
     </>
   );
 };
+
+export default Home;
