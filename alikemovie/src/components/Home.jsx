67413aefc53deb935cea535e7a2d8ab0836aa3dc
MIINII <ft.dawn.o@gmail.com>
2022-08-27 10:14:47 +0900

    다윤님 리스트 모달 구현 머지

--- alikemovie/src/components/Home.jsx
+++ alikemovie/src/components/Home.jsx
@@ -1,16 +1,32 @@
-import Reacts from 'react';
+import { useState } from 'react';
 import './Home.css';
 import Logo from '../assets/logo.svg';
-import OrangeArrow from '../assets/arrow_orange.svg';
+import ModalReview from './Modals/ModalReview';
 
 const Home = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const onClickButton = () => {
+    setIsOpen(true);
+  };
+
   return (
     <>
       <div className='home_wrap'>
         <img className='logo' src={Logo} alt='로고' />
-        <div className="list_button">
+        <div className='list_button'>
           <button className='movie_list'>지금까지의 영화들</button>
-          <button className='review_list'>리뷰쓰러 가기</button>
+          <button className='review_list' onClick={onClickButton}>
+            리뷰쓰러 가기
+          </button>
+          {isOpen && (
+            <ModalReview
+              open={isOpen}
+              onClose={() => {
+                setIsOpen(false);
+              }}
+            />
+          )}
         </div>
       </div>
     </>
