ce6b46f8a3d814012686e6aa3284bb5b3e7ff881
MIINII <83333217+MIINII@users.noreply.github.com>
2022-08-27 15:22:52 +0900

    바보

--- alikemovie/src/components/Modals/ModalInput.jsx
+++ alikemovie/src/components/Modals/ModalInput.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import "./App.css";
 import Modal from "react-modal";
@@ -79,94 +78,67 @@ function ModalInput() {
   );
 }
 
-=======
-import React from 'react';
-import styled from 'styled-components';
 
-function ModalInput({ onClose }) {
-  const handleClose = () => {
-    onClose?.();
-  };
-  return (
-    <Overlay>
-      <ModalWrap>
-        <CloseButton onClick={handleClose}>
-          <i className='fa-solid fa-xmark'></i>
-        </CloseButton>
-        <Contents>
-          <h1> 영화 리뷰 상세내용 </h1>
-          <h1> 글쓴이 </h1>
-          <h1> 재미있어요 </h1>
+// const Overlay = styled.div`
+//   position: fixed;
+//   width: 100%;
+//   height: 100%;
+//   top: 0;
+//   bottom: 0;
+//   left: 0;
+//   right: 0;
+//   background: rgba(0, 0, 0, 0.2);
+//   z-index: 9999;
+// `;
 
-          <Button onClick={handleClose}>Close</Button>
-        </Contents>
-      </ModalWrap>
-      <div> test</div>
-    </Overlay>
-  );
-}
+// const ModalWrap = styled.div`
+//   width: 600px;
+//   height: fit-content;
+//   border-radius: 15px;
+//   background-color: #fff;
+//   position: absolute;
+//   top: 50%;
+//   left: 50%;
+//   transform: translate(-50%, -50%);
+// `;
 
-const Overlay = styled.div`
-  position: fixed;
-  width: 100%;
-  height: 100%;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  background: rgba(0, 0, 0, 0.2);
-  z-index: 9999;
-`;
+// const CloseButton = styled.div`
+//   float: right;
+//   width: 40px;
+//   height: 40px;
+//   margin: 20px;
+//   cursor: pointer;
+//   i {
+//     color: #5d5d5d;
+//     font-size: 30px;
+//   }
+// `;
 
-const ModalWrap = styled.div`
-  width: 600px;
-  height: fit-content;
-  border-radius: 15px;
-  background-color: #fff;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-`;
+// const Contents = styled.div`
+//   margin: 50px 30px;
+//   h1 {
+//     font-size: 30px;
+//     font-weight: 600;
+//     margin-bottom: 60px;
+//   }
+//   img {
+//     margin-top: 60px;
+//     width: 300px;
+//   }
+// `;
+// const Button = styled.button`
+//   font-size: 14px;
+//   padding: 10px 20px;
+//   border: none;
+//   background-color: #ababab;
+//   border-radius: 10px;
+//   color: white;
+//   font-style: italic;
+//   font-weight: 200;
+//   cursor: pointer;
+//   &:hover {
+//     background-color: #898989;
+//   }
+// `;
 
-const CloseButton = styled.div`
-  float: right;
-  width: 40px;
-  height: 40px;
-  margin: 20px;
-  cursor: pointer;
-  i {
-    color: #5d5d5d;
-    font-size: 30px;
-  }
-`;
-
-const Contents = styled.div`
-  margin: 50px 30px;
-  h1 {
-    font-size: 30px;
-    font-weight: 600;
-    margin-bottom: 60px;
-  }
-  img {
-    margin-top: 60px;
-    width: 300px;
-  }
-`;
-const Button = styled.button`
-  font-size: 14px;
-  padding: 10px 20px;
-  border: none;
-  background-color: #ababab;
-  border-radius: 10px;
-  color: white;
-  font-style: italic;
-  font-weight: 200;
-  cursor: pointer;
-  &:hover {
-    background-color: #898989;
-  }
-`;
-
->>>>>>> a66527099a0bd2198738f87ebfa44f5c9403e3e8
 export default ModalInput;
