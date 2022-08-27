24fcfa2e973552ff7dba3ccb21e35d778c0fbdfb
MIINII <ft.dawn.o@gmail.com>
2022-08-27 10:17:41 +0900

    아무것도 없음

--- alikemovie/src/components/Home.jsx
+++ alikemovie/src/components/Home.jsx
@@ -1,32 +1,16 @@
-import { useState } from 'react';
+import Reacts from 'react';
 import './Home.css';
 import Logo from '../assets/logo.svg';
-import ModalReview from './Modals/ModalReview';
+import OrangeArrow from '../assets/arrow_orange.svg';
 
 const Home = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const onClickButton = () => {
-    setIsOpen(true);
-  };
-
   return (
     <>
       <div className='home_wrap'>
         <img className='logo' src={Logo} alt='로고' />
-        <div className='list_button'>
-          <button className='movie_list'>지금까지의 영화들</button>
-          <button className='review_list' onClick={onClickButton}>
-            리뷰쓰러 가기
-          </button>
-          {isOpen && (
-            <ModalReview
-              open={isOpen}
-              onClose={() => {
-                setIsOpen(false);
-              }}
-            />
-          )}
+        <div className="list_button">
+          <button onClick="" className='movie_list'>지금까지의 영화들</button>
+          <button onClick="" className='review_list'>리뷰쓰러 가기</button>
         </div>
       </div>
     </>
