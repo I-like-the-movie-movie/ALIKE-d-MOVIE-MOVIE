c2d7601b82e27607e29aa9caacaccc0d5538c2c9
wildgallop21 <wildgallop21@gmail.com>
2022-08-27 13:43:47 +0900

    모달 작서 및 기존 팀원 병합

--- alikemovie/src/App.js
+++ alikemovie/src/App.js
@@ -1,23 +1,23 @@
 import React, { useSelector } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
+import List from './components/List';
+
+// const App = () => {
+//   return (
+//     <div>
+//       {' '}
+//       <Router />{' '}
+//     </div>
+//   );
+// };
 
 const App = () => {
   return (
-    <div>
-      {' '}
-      <Router />{' '}
-    </div>
-  );
-};
-
-const Router = () => {
-  // const todos = useSelector(state => state.todos.todos);
-  // console.log(todos);
-  return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/list' element={<List />}></Route>
       </Routes>
     </BrowserRouter>
   );
