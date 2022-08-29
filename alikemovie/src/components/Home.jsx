d6ec79d790b9656277ecf8681d1013af1de0a96c
MIINII <83333217+MIINII@users.noreply.github.com>
2022-08-29 10:27:31 +0900

    수정

--- alikemovie/src/components/Home.jsx
+++ alikemovie/src/components/Home.jsx
@@ -1,5 +1,4 @@
-
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Home.css';
 import Logo from '../assets/logo.svg';
@@ -7,22 +6,22 @@ import Logo from '../assets/logo.svg';
 import ModalInput from './Modals/ModalInput';
 import List from './List';
 import styled from 'styled-components';
-import axios from "axios"; 
+import axios from 'axios';
 
 const Home = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [review, setReview] = useState({
-    movie_title: "",
+    movie_title: '',
   });
   const [reviews, setReviews] = useState(null);
 
   const fetchReviews = async () => {
-    const { data } = await axios.get("http://localhost:3001/reviews");
+    const { data } = await axios.get('http://localhost:3001/reviews');
     setReviews(data);
   };
 
-  const onSubmitHandler = (review) => {
-    axios.post("http://localhost:3001/reviews", review);
+  const onSubmitHandler = review => {
+    axios.post('http://localhost:3001/reviews', review);
   };
 
   useEffect(() => {
@@ -41,9 +40,11 @@ const Home = () => {
         <img className='logo' src={Logo} alt='로고' />
 
         <div className='list_button'>
-          <button className='now_movie_list' onClick={() => {
-            navigate('/list')
-          }}>
+          <button
+            className='now_movie_list'
+            onClick={() => {
+              navigate('/list');
+            }}>
             지금까지의 영화들-
           </button>
 
@@ -64,25 +65,24 @@ const Home = () => {
   );
 };
 
-            const Button = styled.button`
-            font-size: 14px;
-            padding: 10px 20px;
-            border: none;
-            background-color: #fa9f98;
-            border-radius: 10px;
-            color: white;
-            font-style: italic;
-            font-weight: 200;
-            cursor: pointer;
-            &:hover {
-              background-color: #fac2be;
-            }
-          `;
-          
-          const AppWrap = styled.div`
-            text-align: center;
-            margin: 50px auto;
-          `;
+const Button = styled.button`
+  font-size: 14px;
+  padding: 10px 20px;
+  border: none;
+  background-color: #fa9f98;
+  border-radius: 10px;
+  color: white;
+  font-style: italic;
+  font-weight: 200;
+  cursor: pointer;
+  &:hover {
+    background-color: #fac2be;
+  }
+`;
 
+const AppWrap = styled.div`
+  text-align: center;
+  margin: 50px auto;
+`;
 
 export default Home;
