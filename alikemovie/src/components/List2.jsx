2acdbda60d09691a8f38baacc0f7ec17e3835a41
MIINII <83333217+MIINII@users.noreply.github.com>
2022-08-31 09:02:27 +0900

    외않되

--- alikemovie/src/components/List2.jsx
+++ alikemovie/src/components/List2.jsx
@@ -1,50 +1,33 @@
-import { PayloadAction } from '@reduxjs/toolkit';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { __getReviews } from '../Redux/modules/reviewSlice';
-// import { reviewsSlice } from "../Redux/modules/reviewSlice";
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import './List2.css';
+import imgfile from '../assets/image.jpg';
+import axios from 'axios';
 
 const List2 = () => {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
+  const [reviewsCon, setReviewsCon] = useState({ movie_title: '', picture: null, contents: '' });
 
-  useEffect(() => {
-    dispatch(__getReviews());
-  }, [dispatch]);
-
-  const { isLoading, error, reviews } = useSelector(state => state.reviews);
-  if (isLoading) {
-    return <div>로딩 중....</div>;
-  }
-
-  if (error) {
-    return <div>{error.message}</div>;
-  }
-  console.log({ reviews });
-  // console.log({ reviewsSlice });
-  // console.log({dispatch})
-  // console.log({PayloadAction})
+  const fetchReviews = async () => {
+    const { reviews } = await axios.get('http://locahost:3001/reviews');
+    setReviewsCon(reviews);
+  };
 
   return (
-    <div>
-      <div>
-        {reviews?.map(review => (
-          <div>
-            <div
-              onClick={() => {
-                navigate(`/review/${review.id}`);
-              }}>
-              <div key={review.id}>{review.movie_title}</div>
-              <div key={review.id}>{review.picture}</div>
-              <div key={review.id}>{review.star}</div>
-            </div>
+    <main className='wrap'>
+      <header className='list_title'>영화입니다</header>
+
+      <article className='movie_list'>
+        <div className='movie_info'>
+          <div className='movie_poster'>
+            <img src={imgfile} alt='이미지포스터' />
           </div>
-        ))}
-      </div>
-      {/* <div>List2</div>;  */}
-    </div>
+          <h3>바보멍청이똥개</h3>
+          <p>존잼</p>
+        </div>
+      </article>
+    </main>
   );
 };
-
 export default List2;
